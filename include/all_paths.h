33430aa462e4dc47ca3d28d63cbae42a2b91864d
Mazen Hassan <mazenby6@gmail.com>
2024-04-28 22:51:47 +0300

    Initial files

---
+++ include/all_paths.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class all_paths
+{
+    public:
+        void view_all_paths();
+        void compute_all_paths();
+};
---
+++ src/all_paths.cpp
@@ -0,0 +1,11 @@
+#include "../include/all_paths.h"
+
+void all_paths::compute_all_paths()
+{
+
+}
+
+void all_paths::view_all_paths()
+{
+
+}
