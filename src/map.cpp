adc10b8736f017bb3c56552a5234901d97cd782c
Radwan <eslamradwan145@gmail.com>
2024-04-29 22:53:37 +0300

    refactoring the adjlist to be unordered_map and edit all files to deal with this change

--- src/map.cpp
+++ src/map.cpp
@@ -9,3 +9,13 @@ using namespace std;
 void Map::bfs() {}
 
 void Map::dfs() {}
+
+int Map::getNumberOfEdges()
+{
+  int cnt = 0;
+  for(auto i:Map::adjList)
+  {
+    cnt += i.second.size();
+  }
+  return cnt/2;
+}
