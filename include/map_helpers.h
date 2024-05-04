9ef313ad9f8bfab3a90eaaca134b10b338c5662a
Mahmoud Bassem <82619789+Mahmoud-Bassem@users.noreply.github.com>
2024-05-04 13:16:49 +0300

    Added map update functionalities\n\n implement the update functions in account.cpp, added comparison operator in RoadProperties class

--- include/map_helpers.h
+++ include/map_helpers.h
@@ -27,6 +27,10 @@ class RoadProperties {
   //
   RoadProperties(int, transportations);
   RoadProperties(const RoadProperties&);
+
+  bool operator==(const RoadProperties &other) {
+    return other.cost == cost && other.transport == transport;
+  }
 };
 
 class Road : Edge {
