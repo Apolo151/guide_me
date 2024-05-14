d6cca5f76ce7c265b68bdea933d143e7542fd203
Radwan <eslamradwan145@gmail.com>
2024-05-14 07:23:20 +0300

    Finish Admin Part

--- include/all_paths.h
+++ include/all_paths.h
@@ -9,21 +9,23 @@
 #include "../include/map_helpers.h"
 using namespace std;
 class AllPaths {
+ public:
+  class Path {
    public:
-    class Path {
-       public:
-        int cost;
-        vector<string> pathVector;
-        Path(int cost, vector<string> pathVector);
-        bool operator<(const Path& other) const;
-    };
+    int cost;
+    vector<vector<string>> pathVector;
+    Path(int cost, vector<vector<string>> pathVector);
+    bool operator<(const Path& other) const;
+  };
 
-    string source, destination;
-    unordered_map<string, unordered_map<string, Route>> graph;
-    vector<pair<int, vector<string>>> allPathsVector;
-    AllPaths(unordered_map<string, unordered_map<string, Route>>& graph, string, string);
-    void displayAllPaths();
-    void computeAllPaths();
-    void dfsAllPaths(string, map<string, bool>&, vector<string>, int);
-    bool comparePaths(Path&, Path&);
+  string source, destination;
+  unordered_map<string, unordered_map<string, Route>> graph;
+  vector<pair<int, vector<vector<string>>>> allPathsVector;  // from to cost
+  AllPaths(unordered_map<string, unordered_map<string, Route>>& graph, string,
+           string);
+  void displayAllPaths();
+  void computeAllPaths(int);
+  void dfsAllPaths(string, map<string, bool>&, vector<vector<string>>, int,
+                   int&);
+  bool comparePaths(Path&, Path&);
 };
--- include/map.h
+++ include/map.h
@@ -1,23 +1,26 @@
 #pragma once
 
+#include <map>
 #include <queue>
 #include <string>
 #include <unordered_map>
-#include<map>
+
 #include "data_manager.h"
 #include "map_helpers.h"
 using namespace std;
 
 class Map {
   Map();
+
  public:
- 
- static int compute_hash(string s);   
- static vector<pair<string, string>> saveCheapestPath(map<string, pair<string, transportations>>, string);
- 	 static int cnt;
-  static map<string, int>hash;
+  static int compute_hash(string s);
+  static vector<pair<string, string>> saveCheapestPath(
+      map<string, pair<string, transportations>>, string);
+  static int cnt;
+  static map<string, int> hash;
   static unordered_map<string, unordered_map<string, Route>> adjList;
-  static pair<long long, vector<pair<string, string>>> Dijkstra(string start,string destination);
+  static pair<long long, vector<pair<string, string>>> Dijkstra(
+      string start, string destination);
   static void bfs(string start);
   static void printSavedTraverse(queue<string> traverseQueue);
   static int getNumberOfEdges();
